575c4367441f4138ab794b8efa6e1cf42e4f62d5
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2017-01-15 15:58:55 -0800

    Assignment 1, before cocoPods

--- flicks/MoviesViewController.swift
+++ flicks/MoviesViewController.swift
@@ -37,8 +37,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = self.movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
+        let baseUrl = "https://image.tmdb.org/t/p/w500/"
+        let imageUrl = NSURL(baseUrl + posterPath)
+        
+        
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        //cell.posterView
         
         return cell
     }
