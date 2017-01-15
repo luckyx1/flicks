9531d276ff4cac535c0c36a25f8d48ebaf778593
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2017-01-15 02:02:04 -0800

    d

---
+++ flicks/MoviesViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MoviesViewController.swift
+//  flicks
+//
+//  Created by Rob Hernandez on 1/15/17.
+//  Copyright © 2017 Robert Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+            return 5
+    }
+    
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        cell.textLabel?.text = "row at index \(indexPath)"
+        return cell
+    }
+    
+
+
+}
