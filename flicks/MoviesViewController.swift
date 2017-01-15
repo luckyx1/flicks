37d8a10134b548119ba5f98becce22e36993e514
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2017-01-15 15:35:44 -0800

    Assignment 1: part 2 completed

--- flicks/MoviesViewController.swift
+++ flicks/MoviesViewController.swift
@@ -11,24 +11,49 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    var movies: [NSDictionary]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        self.getNowFeaturing()
 
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-            return 5
+        if let movies = movies {
+            return movies.count
+        }else{
+            return 0
+        }
     }
     
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
-        cell.textLabel?.text = "row at index \(indexPath)"
+        
+        let movie = self.movies![indexPath.row]
+        let title = movie["title"] as! String
+        cell.textLabel?.text = title
         return cell
+    }
+    
+    func getNowFeaturing(){
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    self.movies = dataDictionary["results"] as! [NSDictionary]
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        task.resume()
     }
     
 
