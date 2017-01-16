33eb5759f12cf201d66c00a79daf9044ec1dac83
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2017-01-16 15:57:40 -0800

    Building Collection View, in progress

---
+++ flicks/MovieCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionCell.swift
+//  flicks
+//
+//  Created by Rob Hernandez on 1/16/17.
+//  Copyright © 2017 Robert Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionCell: UICollectionViewCell {
+    
+}
---
+++ flicks/MovieCollectionViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MovieCollectionViewController.swift
+//  flicks
+//
+//  Created by Rob Hernandez on 1/16/17.
+//  Copyright © 2017 Robert Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("fish", forIndexPath: indexPath) as! MovieCollectionCell
+
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
